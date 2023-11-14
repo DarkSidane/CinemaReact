c6bfc0e9fd9360ea9954d35896df532f655fe93f
Darksidane <sidanealp0@gmail.com>
2023-11-14 17:08:48 +0100

    display OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+// Component to display individual movie cards
+const MovieCard = ({ movie }) => {
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <a
+      href={`https://yourdomain.com/movie-list-vite/movie/${movie.id}`}
+      className="group block w-full rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        className="w-full transform transition-transform duration-300 ease-in-out group-hover:scale-110"
+        src={imageUrl}
+        alt={movie.title}
+      />
+    </a>
+  );
+};
 
-export default App
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing', {
+          headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTY3OTRlMDU1ZjRiMDA0OWVkYjAwNzYwNmU3YTJiMCIsInN1YiI6IjY1NGE1MDM3MWFjMjkyN2IyZjI3MjgxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Zh5RQtn5g1oHuAfOyZmiNqBgPdAp5MWxY3jYPoJdjqM'
+          }
+        });
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error('Failed to fetch movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="bg-gray-800 p-8 text-white text-3xl font-bold text-center">
+        🎬🍿 Movie library
+      </header>
+      <main className="p-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+          {movies.map(movie => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
