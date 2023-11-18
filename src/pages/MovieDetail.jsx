4cd98eedff77e5363fd72b8ad6814b198550dad7
DarkSidane <sidanealp0@gmail.com>
2023-11-18 07:25:55 +0100

    movie Details marche

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -4,17 +4,17 @@ import { useParams, Link } from 'react-router-dom';
 const MovieDetail = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
-
-  useEffect(() => {
+   useEffect(() => {
     const fetchMovieDetail = async () => {
       try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=en-US`, {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?append_to_response=credits&language=en-US`, {
           headers: {
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTY3OTRlMDU1ZjRiMDA0OWVkYjAwNzYwNmU3YTJiMCIsInN1YiI6IjY1NGE1MDM3MWFjMjkyN2IyZjI3MjgxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Zh5RQtn5g1oHuAfOyZmiNqBgPdAp5MWxY3jYPoJdjqM'
           }
         });
         const data = await response.json();
         setMovie(data);
+        setCredits(data.credits); // Adjust according to your API response
       } catch (error) {
         console.error('Error fetching movie details:', error);
       }
@@ -23,34 +23,32 @@ const MovieDetail = () => {
     fetchMovieDetail();
   }, [movieId]);
 
-  if (!movie) {
-    return <div className="flex justify-center items-center h-screen">Loading...</div>;
-  }
-
-  const posterUrl = movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : '';
-  const backUrl = movie.backdrop_path ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}` : '';
+  const posterUrl = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'placeholder-image-url';
+  const backdropUrl = movie.backdrop_path ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}` : 'placeholder-backdrop-url';
 
   return (
-    <div className={`bg-cover bg-center`} style={{ backgroundImage: `url(${backUrl})` }}>
-      <div className="relative p-4">
-        <Link to="/" className="inline-block px-4 py-2 bg-gray-300 rounded-full">
-          ← Back
+    <main style={{ backgroundImage: `url(${backdropUrl})` }} className="flex min-h-screen min-w-full bg-cover ">
+      <div className="flex-1 flex flex-col gap-4 backdrop-blur-2xl bg-slate-800/40 p-6 lg:p-12 overflow-hidden">
+        <Link to="/" className="self-start transition ease-in-out text-white flex gap-1 items-center hover:-translate-x-2">
+          {/* Icon and Back button */}
         </Link>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {posterUrl && <img className="max-w-sm h-auto rounded-lg shadow-lg" src={posterUrl} alt={movie.title} />}
-          <div className="bg-black bg-opacity-70 rounded-2xl p-5 text-white">
-            <h1 className="text-4xl">{movie.title}</h1>
-            <p>{movie.overview}</p>
-            {movie.genres && (
-              <p className="italic text-gray-400">
-                {movie.genres.map(genre => genre.name).join(', ')}
-              </p>
-            )}
-            <p className="text-gray-300">{new Date(movie.release_date).toLocaleDateString()}</p>
+        <div className="flex flex-col items-center flex-wrap gap-4 justify-center sm:flex-row sm:items-end">
+          <img className="rounded-md object-cover" src={posterUrl} alt={movie.title} width="300" />
+          <div className="flex flex-col flex-1">
+            <p className="text-4xl w-auto text-white">{movie.title}</p>
+            <p className="text-md w-auto text-white">{movie.overview}</p>
+            {/* Other movie details like genres and release date */}
           </div>
         </div>
+        <div>
+          <p className="text-3xl text-white">Credits</p>
+          <div className="flex gap-6 pt-2 overflow-x-scroll">
+            {/* Credits images and names */}
+          </div>
+        </div>
+        {/* Images section if needed */}
       </div>
-    </div>
+    </main>
   );
 };
 
